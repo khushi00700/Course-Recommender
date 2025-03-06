10a4c6145145ca7b16f54d46ab89c11e986f04d1
Khushi Panchal <khushimpanchal03@gmail.com>
2025-03-06 16:17:16 +0530

    updated routes and controller

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,18 +1,36 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+import dotenv from 'dotenv';
 
-const authMiddleware = (req, res, next) => {
-  const token = req.header('Authorization');
-  if (!token) {
-    return res.status(401).send('Access Denied');
+dotenv.config();
+
+const protect = async (req, res, next) => {
+  let token = req.header('Authorization');
+
+  if(!token) {
+    return res.status(401).json({ message: "Access denied! No token provided" });
   }
 
-  try {
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.user = decoded;
+  try{
+    // Ensure token starts with 'Bearer'
+    if(token.startsWith('Bearer')) {
+      token = token.split(' ')[1];
+    }
+
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Attach user to request excluding password
+    req.user = await User.findById(decoded.id).select('-password');
+
+    if(!req.user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
     next();
-  } catch (err) {
-    res.status(400).send('Invalid Token');
+  } catch(error) {
+    res.status(401).json({ message: "Access denied! Invalid token" });
   }
 }
 
-export default authMiddleware;
+export default protect;
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import { registerUser, loginUser } from '../controllers/userController.js';
+import { registerUser, loginUser, getUserProfile, updateUserSkills } from '../controllers/userController.js';
+import protect from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
+// Get user profile
+router.get('/profile', protect, getUserProfile);
+
+// Update user skills and interests
+router.put('/update-skills',protect , updateUserSkills);
 
 export default router;
